70c245692fb7cdcf458933544c88dbc9abbc7bb7
fernrey <fernando39@live.com>
2025-11-13 22:13:46 -0500

    sample code to test out ball staying in frame

--- tilt_maze_game/tilt_maze_game.ino
+++ tilt_maze_game/tilt_maze_game.ino
@@ -1,84 +1,48 @@
 #include <TinyScreen.h>
-#include <Wire.h>
-#include <SPI.h>
 #include "TinyArcade.h"
-#include "GameTutorialSprites.h"
-
-const uint16_t ALPHA = 0x1111;
 
 TinyScreen display = TinyScreen(TinyScreenPlus);
 
-typedef struct {
-  int x;
-  int y;
-  int width;
-  int height;
-  int collisions;
-  const unsigned int *bitmap;
-} ts_sprite;
+int ballX = 45;
+int ballY = 30;
 
-ts_sprite ball = {44,28,4,4,0, ballBitmap};
-
-int amtSprites = 1;
-
-ts_sprite * spriteList[1] = {&ball};
-
-int backgroundColor = TS_16b_Black;
-
-void setup () {
-  arcadeInit();
+void setup() {
+  arcadeInit();  // MUST call this first to setup joystick
   display.begin();
-  display.setBitDepth(TSBitDepth16);
   display.setBrightness(15);
   display.setFlip(false);
-
-USBDevice.init();
-USBDevice.attach();
-SerialUSB.begin(9600);
-}
-
-void loop(){
-  drawBuffer();
-
-if (checkJoystick(TAJoystickUp))    ball.y -= 1;
-if (checkJoystick(TAJoystickDown))  ball.y += 1;
-if (checkJoystick(TAJoystickLeft))  ball.x -= 1;
-if (checkJoystick(TAJoystickRight)) ball.x += 1;
-if (checkButton(TAButton1)) ball.x = 0;
-if (checkButton(TAButton2)) ball.y = 0;
-}
-
-void drawBuffer() {
-  uint8_t lineBuffer[96 * 64 * 2];
-  display.startData();
   
-  for (int y = 0; y < 64; y++) {
-    for (int b = 0; b < 96; b++) {
-      lineBuffer[b * 2] = backgroundColor >> 8;
-      lineBuffer[b * 2 + 1] = backgroundColor;
-    }
-    
-    for (int spriteIndex = 0; spriteIndex < amtSprites; spriteIndex++) {
-      ts_sprite *cs = spriteList[spriteIndex];
-      if (y >= cs->y && y < cs->y + cs->height) {
-        int endX = cs->x + cs->width;
-        if (cs->x < 96 && endX > 0) {
-          int xBitmapOffset = 0;
-          int xStart = 0;
-          if (cs->x < 0) xBitmapOffset -= cs->x;
-          if (cs->x > 0) xStart = cs->x;
-          int yBitmapOffset = (y - cs->y) * cs->width;
-          for (int x = xStart; x < endX; x++) {
-            unsigned int color = cs->bitmap[xBitmapOffset + yBitmapOffset++];
-            if (color != ALPHA) {
-              lineBuffer[(x) * 2] = color >> 8;
-              lineBuffer[(x) * 2 + 1] = color;
-            }
-          }
-        }
-      }
-    }
-    display.writeBuffer(lineBuffer, 96 * 2);
+  // Draw blue background
+  display.drawRect(0, 0, 96, 64, TSRectangleFilled, TS_8b_Blue);
+}
+
+void loop() {
+  // Erase old ball (draw blue over it)
+  display.drawRect(ballX, ballY, 6, 6, TSRectangleFilled, TS_8b_Blue);
+  
+  // Check each direction and move ball
+  if (checkJoystick(TAJoystickUp)) {
+    ballY -= 2;  // Move up
   }
-  display.endTransfer();
+  if (checkJoystick(TAJoystickDown)) {
+    ballY += 2;  // Move down
+  }
+  if (checkJoystick(TAJoystickLeft)) {
+    ballX -= 2;  // Move left
+  }
+  if (checkJoystick(TAJoystickRight)) {
+    ballX += 2;  // Move right
+  }
+
+  // Keep ball on screen (boundary checks)
+  if (ballX < 0) ballX = 0;           // Left edge
+  if (ballX > 90) ballX = 90;         // Right edge (96 - 6 = 90)
+  if (ballY < 0) ballY = 0;           // Top edge
+  if (ballY > 58) ballY = 58;         // Bottom edge (64 - 6 = 58)
+
+  
+  // Draw ball at new position (red square)
+  display.drawRect(ballX, ballY, 6, 6, TSRectangleFilled, TS_8b_Red);
+  
+  delay(50);
 }
